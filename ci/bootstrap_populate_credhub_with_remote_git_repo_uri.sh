082615223c6abf419369746d134a991f3d7db47a
matthewcosgrove <mc.matthew.cosgrove@gmail.com>
2019-12-28 09:50:49 +0000

    Minor fix for default pipeline name var

--- ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
+++ ci/bootstrap_populate_credhub_with_remote_git_repo_uri.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -eu
 
-PIPELINE_NAME=${PIPELINE_NAME:-opsman-vm-installs}
+PIPELINE_NAME=${PIPELINE_NAME:-pcf-opsman-vm-installs}
 credhub set -t value -n /concourse/main/$PIPELINE_NAME/git_repo_uri -v $GIT_REPO_URI
 credhub set -t value -n /concourse/main/$PIPELINE_NAME/git_opsman_vm_state_repo_uri -v $STATE_GIT_REPO_URI
